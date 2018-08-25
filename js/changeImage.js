552c748322977424f2363e25d9ffa1365af6fee1
A S KARTHIK SAI VISHNU KUMAR <stark@As-MacBook-Pro.local>
2018-08-25 10:46:37 +0530

    ->added materialize css and js files. ->changed the file structure. ->changed the alignment of components in the window.

---
+++ js/changeImage.js
@@ -0,0 +1,12 @@
+//Has not been used anywhere
+
+//  require(getImage.js) [wrong syntax]
+      //  console.log(imageArray[0]);
+      //  var myImage=document.getElementById('mainImage');
+        
+      //  var imageIndex=1;
+     
+      //  function Image(){
+      //    myImage.setAttribute("src",imageArray[imageIndex]);
+      //    imageIndex++;
+      //  }
